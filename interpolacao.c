9f22235eb86efa35f1a149ec8ce921190ea39055
SophiaSilveira <sophiasilveira2003@gmail.com>
2025-06-07 16:12:20 -0300

    feat: basic info

---
+++ interpolacao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int  vet_seconds[7] = {  0, 0.5, 0.8,   1, 1.4, 2, 2.7};
+    int  vet_meters[7]  = {1.6,   3, 3.7, 4.8, 4.7, 3, 2.6};
+
+    int count = 7; 
+
+
+    return 0;
+}
