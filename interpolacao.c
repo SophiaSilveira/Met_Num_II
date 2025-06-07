bd6afec3b20df994607062d09adb9534dcaf8438
SophiaSilveira <sophiasilveira2003@gmail.com>
2025-06-07 17:11:55 -0300

    fix: correcting variables names

--- interpolacao.c
+++ interpolacao.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float lagrange(int size, float *s[], float *m[]){
+
+    return 0;
+};
+
 int main(){
-    int  vet_seconds[7] = {  0, 0.5, 0.8,   1, 1.4, 2, 2.7};
-    int  vet_meters[7]  = {1.6,   3, 3.7, 4.8, 4.7, 3, 2.6};
+    float  vet_seconds[7] = {  0, 0.5, 0.8,   1, 1.4, 2, 2.7};
+    float  vet_meters[7]  = {1.6,   3, 3.7, 4.8, 4.7, 3, 2.6};
 
-    int count = 7; 
+    int vet_size = 7; 
 
+    float result = lagrange();
 
     return 0;
 }
