46c8e23b6eea88756940dfe8e10ef696debd14a7
SophiaSilveira <sophiasilveira2003@gmail.com>
2025-06-07 18:05:01 -0300

    feat: lagrange method

--- interpolacao.c
+++ interpolacao.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float lagrange(int size, float *s[], float *m[]){
+double lagrange(double x, double s[], double m[], int size){
 
-    return 0;
+    double total = 0.0;
+
+    for(int i = 0; i < size; i++){
+        double numerador = 1.0;
+        double denominador = 1.0;
+
+        for(int j = 0; j < size; j++){
+            if(i == j) continue;
+            numerador *= (x - s[j]);
+            denominador *= (s[i] - s[j]);
+        }
+
+        total += m[i] * (numerador/denominador);
+    }
+
+    return total;
 };
 
 int main(){
-    float  vet_seconds[7] = {  0, 0.5, 0.8,   1, 1.4, 2, 2.7};
-    float  vet_meters[7]  = {1.6,   3, 3.7, 4.8, 4.7, 3, 2.6};
+    double  vet_seconds[7] = {  0, 0.5, 0.8,   1, 1.4, 2, 2.7};
+    double  vet_meters[7]  = {1.6,   3, 3.7, 4.8, 4.7, 3, 2.6};
 
     int vet_size = 7; 
 
-    float result = lagrange();
+    double second = 1.3;
+
+    double result = lagrange(second, vet_seconds, vet_meters, vet_size);
+
+    printf("Pelo metodo de lagrange for obtido %lf \n", result);
 
     return 0;
 }
